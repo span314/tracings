4d03bb867aae6bc06aa83f61106a7a1f9591fdd9
Shawn Pan <shawnpan@alum.mit.edu>
2016-04-23 15:52:19 -0400

    Convert to JQueryUI widget

--- tracings.js
+++ tracings.js
@@ -1,19 +1,51 @@
-var drawOnCanvas = function(canvas, dance) {
-  if (canvas.getContext) {
-    var centerX = canvas.width / 2;
-    var centerY = canvas.height / 2;
-  	var ctx = canvas.getContext('2d');
-    var offsetAngle = Math.PI / 2;
+$(document).ready(function() {
+  $('#diagramCanvas').diagram({danceUrl: 'canasta_tango.json'});
+});
+
+$.widget('shawnpan.diagram', {
+  options: {
+    danceUrl: 'canasta_tango.json'
+  },
+
+  _create: function() {
+      console.log('widget create');
+      var canvas = this.canvas = this.element.get(0);
+      if (!canvas.getContext) {
+        console.log('Canvas not supported');
+        return;
+      }
+      this.canvasContext = canvas.getContext('2d');
+      this._onCanvasResize();
+      this._loadDance();
+  },
+
+  _onCanvasResize: function() {
+    this.centerX = this.canvas.width / 2;
+    this.centerY = this.canvas.height / 2;
+    this.offsetAngle = Math.PI / 2 * (this.canvas.width > this.canvas.height);
+  },
+
+  _loadDance: function() {
+    var widget = this;
+    $.getJSON(this.options.danceUrl, function(data) {
+      console.log(data);
+      widget.dance = data;
+      widget._drawPattern();
+    });
+  },
+
+  _drawPattern: function() {
+    var ctx = this.canvasContext;
     var pattern, component, path;
     var patternIndex, componentIndex, pathIndex, lapIndex;
 
     patternIndex = 0;
-    pattern = dance.patterns[patternIndex];
+    pattern = this.dance.patterns[patternIndex];
 
-    for (lapIndex = 0; lapIndex < dance.patternsPerLap; lapIndex++) {
+    for (lapIndex = 0; lapIndex < this.dance.patternsPerLap; lapIndex++) {
       ctx.save()
-      ctx.translate(centerX, centerY);
-      ctx.rotate(offsetAngle + 2 * Math.PI * lapIndex / dance.patternsPerLap);
+      ctx.translate(this.centerX, this.centerY);
+      ctx.rotate(this.offsetAngle + 2 * Math.PI * lapIndex / this.dance.patternsPerLap);
       for (componentIndex = 0; componentIndex < pattern.components.length; componentIndex++) {
         component = pattern.components[componentIndex];
         for (pathIndex = 0; pathIndex < component.paths.length; pathIndex++) {
@@ -26,19 +58,6 @@ var drawOnCanvas = function(canvas, dance) {
       }      
       ctx.restore()
     }
-  	console.log('done');
-  } else {
-  	console.log('Canvas not supported');
+    console.log('done');
   }
-}
-
-$(document).ready(function() {
-  $.getJSON('canasta_tango.json', function(data) {
-    console.log(data);
-
-    var canvas = $('#diagramCanvas').get(0);
-    drawOnCanvas(canvas, data);
-  });
 });
-
-console.log("foo");
