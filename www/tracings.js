fe41f8c42e089ef1969e483a0fb20ed00e06e48c
Shawn Pan <shawnpan@alum.mit.edu>
2016-05-08 23:08:00 -0400

    Adding beat animation

--- www/tracings.js
+++ www/tracings.js
@@ -1,4 +1,5 @@
 //Requires IE9 or later - uses HTML5 and ECMAScript5
+'use strict';
 
 $(document).ready(function() {
   $('#diagramContainer').diagram();
@@ -106,8 +107,8 @@ $.widget('shawnpan.diagram', {
   },
 
   _loadPattern: function() {
-    var i
-        optionalSteps = this.controls.optional.prop('checked') ? 'yes' : 'no';;
+    var i, pattern,
+        optionalSteps = this.controls.optional.prop('checked') ? 'yes' : 'no';
     console.log('loading pattern ' + this.dance.name + ' ' + this.part);
     for (i = 0; i < this.dance.patterns.length; i++) {
       pattern = this.dance.patterns[i];
@@ -127,15 +128,14 @@ $.widget('shawnpan.diagram', {
   _drawPattern: function() {
     var pattern, component, path, lapIndex, componentIndex, pathIndex,
         ctx = this.canvasContext,
+        currentComponent = this._currentComponent(),
+        currentLap = Math.floor(this.position / this.components.length),
         tickCount = this._currentComponent().offset + this.stepTickCount,
         fracBeat = tickCount % 4,
         beat = ((tickCount - fracBeat) / 4) % this.dance.timeSignatureTop + 1;
 
     ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    ctx.fillRect(0, 0, beat * this.canvas.width / this.dance.timeSignatureTop, 10);
-
-
-    ctx.lineWidth = 3;
+    //ctx.fillRect(0, 0, beat * this.canvas.width / this.dance.timeSignatureTop, 10);
 
     for (lapIndex = 0; lapIndex < this.dance.patternsPerLap; lapIndex++) {
       ctx.save();
@@ -144,8 +144,23 @@ $.widget('shawnpan.diagram', {
       for (componentIndex = 0; componentIndex < this.components.length; componentIndex++) {
         component = this.components[componentIndex];
         ctx.save();
-        if (this.position === lapIndex * this.components.length + componentIndex) {
-          ctx.strokeStyle = 'rgb(' + (200 + 55 * (fracBeat === 0)) + ',0,0)';
+        if (currentLap === lapIndex) {
+          ctx.lineWidth = 3;
+          if (component === currentComponent) {
+            if (beat === 1 && fracBeat === 0) {
+              ctx.lineWidth = 5;
+              ctx.strokeStyle = 'rgb(255,0,0)';
+            } else if (fracBeat === 0) {
+              ctx.lineWidth = 4;
+              ctx.strokeStyle = 'rgb(235,0,0)';
+            } else {
+              ctx.strokeStyle = 'rgb(215,0,0)';
+            }
+          } else if (component.group && component.group === currentComponent.group) {
+            ctx.strokeStyle = 'rgb(150,0,0)';
+          }
+        } else {
+          ctx.lineWidth = 2;
         }
         for (pathIndex = 0; pathIndex < component.path.length; pathIndex++) {
           path = component.path[pathIndex];
