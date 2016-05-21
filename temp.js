783e9e1c85f4ffea0a1d938d9dc764dba76bd058
Shawn Pan <shawnpan@alum.mit.edu>
2016-05-21 11:51:21 -0400

    Remove ignored files

---
+++ temp.js
@@ -0,0 +1,11 @@
+var PatternAndPosition = function(components) {
+  this.stepTickCount = 0;
+  this.position = 0;
+  this.components = components;
+};
+
+// var Tempo = function(tempoObject) {
+//   this.timeSignatureTop = tempoObject.timeSignatureTop;
+//   this.timeSignatureBottom = tempoObject.timeSignatureBottom;
+//   this.beatsPerMinute = tempoObject.beatsPerMinute;
+// };
