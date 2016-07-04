3ab8cf305a98a4224f2396501b25e1be3099f756
Shawn Pan <shawnpan@alum.mit.edu>
2016-07-03 22:01:38 -0400

    Minify css

--- minify.sh
+++ minify.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-uglifyjs www/tracings.js www/icediagram.js --output www/tracings-all.min.js --compress drop-console --mangle --screw-ie8 --comments "/!.*/" --verbose --source-map www/tracings-all.min.js.map --prefix relative --mangle-props --mangle-regex="/^_/" --reserve-domprops
+cd www;
+uglifyjs tracings.js icediagram.js --output tracings-all.min.js --compress drop-console --mangle --screw-ie8 --comments "/!.*/" --verbose --source-map tracings-all.min.js.map --prefix relative --mangle-props --mangle-regex="/^_/" --reserve-domprops;
+cleancss -o style-all.min.css style.css --source-map;
