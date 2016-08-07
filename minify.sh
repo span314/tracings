ec5ddb47e9a65b02405d7f6fd0388a36d4c1ed50
Shawn Pan <shawnpan@alum.mit.edu>
2016-08-07 16:29:32 -0400

    Shifting to npm build

--- minify.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cd www;
-uglifyjs header.js tracings.js icediagram.js fullscreen-api-polyfill.js --output tracings-all.min.js --compress drop-console --mangle --screw-ie8 --comments "/!.*/" --verbose --source-map tracings-all.min.js.map --prefix relative --mangle-props --mangle-regex="/^_/" --reserve-domprops;
-cleancss -o style-all.min.css style.css --source-map;
-
-#svg
-#scour -i clock-fast-75.svg -o clock-fast-75-s.svg --enable-viewboxing --enable-id-stripping --enable-comment-stripping --shorten-ids --indent=none
