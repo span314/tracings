e4be977fed1ec8774275a33e38a444b255bf2535
Shawn Pan <shawnpan@alum.mit.edu>
2016-08-07 17:56:19 -0400

    Moving directories

--- www/header.js
+++
@@ -1,5 +0,0 @@
-/*!
-This compressed file contains several modules under different compatable
-licenses as commented before each module. Source and full license text
-are available in the source-mapped files and links to the projects.
-*/
