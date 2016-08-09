8c2fb62fcb7522904cebc3b94e2608dbdaaed2ab
Shawn Pan <shawnpan@alum.mit.edu>
2016-08-09 12:53:22 -0400

    Up-ing versions

--- js/tracings.js
+++ js/tracings.js
@@ -1,5 +1,5 @@
 /*!
-Load widgets and bind events v0.2.0 | Software Copyright (c) Shawn Pan
+Load widgets and bind events v0.2.1 | Software Copyright (c) Shawn Pan
 */
 document.addEventListener('DOMContentLoaded', function() {
   'use strict';
